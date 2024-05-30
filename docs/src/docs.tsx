92da0060615d1d7c5cbad9459e6f64f82e2148a6
Simon Fishel <simonf@gmail.com>
2024-05-29 21:36:42 -0700

    more styling and adding navigation

--- docs/src/docs.tsx
+++ docs/src/docs.tsx
@@ -2,6 +2,7 @@ import { javascript } from "@codemirror/lang-javascript";
 import { debounce } from "@solid-primitives/scheduled";
 // @ts-ignore
 import { CodeMirror } from "@solid-codemirror/codemirror";
+import { Router, Route, A } from "@solidjs/router";
 import { minimalSetup } from "codemirror";
 import jsCodeShiftCore from "jscodeshift/src/core";
 import * as prettier from "prettier";
@@ -9,7 +10,6 @@ import * as typescriptPlugin from "prettier/plugins/typescript";
 import * as estreePlugin from "prettier/plugins/estree";
 import { createSignal, createEffect, JSX } from "solid-js";
 import { render } from "solid-js/web";
-import { Tab, TabGroup, TabList, TabPanel } from "terracotta";
 import transform from "../../src/transform";
 import "./docs.css";
 
@@ -29,7 +29,48 @@ const transpile = async (source: string) => {
   });
 };
 
-const MainSection = () => {
+const AppShell = (props: { children: JSX.Element }) => (
+  <div
+    class={`w-full h-screen flex flex-col bg-sky-800 bg-opacity-5 p-6`}
+  >
+    <nav class="flex items-center justify-between">
+      <div class="flex items-center flex-shrink-0">
+        <span class="font-semibold text-3xl tracking-tight">
+          Moment &rarr; Temporal
+        </span>
+      </div>
+      <div class="flex mr-16">
+        <div class="mr-8">
+          <A
+            class={classNames(
+              "font-semibold p-1",
+              `focus:outline-none focus:text-sky-800 focus:ring-1 focus:ring-sky-800 focus:ring-offset-2 focus:rounded`,
+              `hover:text-sky-800`,
+            )}
+            href="/explorer"
+          >
+            Explorer
+          </A>
+        </div>
+        <div>
+          <A
+            class={classNames(
+              "font-semibold p-1",
+              `focus:outline-none focus:text-sky-800 focus:ring-1 focus:ring-sky-800 focus:ring-offset-2 focus:rounded`,
+              `hover:text-sky-800`,
+            )}
+            href="/docs"
+          >
+            Docs
+          </A>
+        </div>
+      </div>
+    </nav>
+    {props.children}
+  </div>
+);
+
+const Explorer = () => {
   const [input, setInput] = createSignal("");
   const [output, setOutput] = createSignal("");
   const debouncedSetOutput = debounce(
@@ -38,85 +79,58 @@ const MainSection = () => {
   );
   createEffect(() => debouncedSetOutput(input()));
   return (
-    <TabGroup
-      class="w-full h-screen flex flex-col items-stretch justify-center p-4 space-y-2 rounded-lg bg-rose-900 bg-opacity-25"
-      defaultValue="explorer"
-      horizontal
-    >
-      {({ isSelected, isActive }): JSX.Element => (
-        <>
-          <TabList class="w-full flex space-x-2">
-            <Tab
-              class={classNames(
-                isSelected("explorer")
-                  ? "bg-rose-900 bg-opacity-75 text-white"
-                  : "bg-white",
-                isActive("explorer") &&
-                  "ring-2 ring-offset-2 ring-offset-rose-300 ring-white ring-opacity-60",
-                "w-28 flex items-center justify-center rounded-lg shadow-md px-4 py-2 cursor-pointer focus:outline-none font-semibold",
-              )}
-              value="explorer"
-            >
-              Explorer
-            </Tab>
-            <Tab
-              class={classNames(
-                isSelected("docs")
-                  ? "bg-rose-900 bg-opacity-75 text-white"
-                  : "bg-white",
-                isActive("docs") &&
-                  "ring-2 ring-offset-2 ring-offset-rose-300 ring-white ring-opacity-60",
-                "w-28 flex items-center justify-center rounded-lg shadow-md px-4 py-2 cursor-pointer focus:outline-none font-semibold",
-              )}
-              value="docs"
-            >
-              Docs
-            </Tab>
-          </TabList>
-          <TabPanel
-            value="explorer"
+    <AppShell>
+      <div class="flex flex-col w-full h-full">
+        <div class="flex w-full h-full py-8 space-x-2">
+          <CodeMirror
+            value={input()}
+            onValueChange={(val: string) => setInput(val)}
+            class={`flex-1 border-sky-800 border rounded p-2 bg-white`}
+            placeholder="Moment.js code goes here"
+            showLineNumbers={false}
+            extensions={[minimalSetup, javascript({ typescript: true })]}
+          />
+          <CodeMirror
+            value={output()}
+            class={`flex-1 border-sky-800 border rounded p-2 bg-white`}
+            placeholder="Moment.js code goes here"
+            showLineNumbers={false}
+            readOnly
+            extensions={[minimalSetup, javascript({ typescript: true })]}
+          />
+        </div>
+        <div class="flex justify-center">
+          <a
             class={classNames(
-              "flex flex-col w-full h-full p-2 rounded-lg bg-white overflow-y-auto focus:outline-none",
-              "focus:ring-2 focus:ring-offset-2 focus:ring-offset-rose-300 focus:ring-white focus:ring-opacity-60",
+              `py-1 px-2 ring-1 rounded ring-sky-800`,
+              `focus:outline-none focus:ring-2 focus:text-sky-800`,
+              `hover:text-sky-800 hover:ring-2`,
             )}
+            href={`https://github.com/sfishel18/moment-to-temporal/issues/new?template=bug-report.yml&title=%5BBug%5D%3A+&input=${encodeURIComponent(input())}&output=${encodeURIComponent(output())}`}
+            target="_blank"
           >
-            <div class="flex w-full h-full px-6 py-8 space-x-2">
-              <CodeMirror
-                value={input()}
-                onValueChange={(val: string) => setInput(val)}
-                class="flex-1 border-slate-500 border rounded p-2"
-                placeholder="Moment.js code goes here"
-                showLineNumbers={false}
-                extensions={[minimalSetup, javascript({ typescript: true })]}
-              />
-              <CodeMirror
-                value={output()}
-                class="flex-1 border-slate-500 border rounded p-2"
-                placeholder="Moment.js code goes here"
-                showLineNumbers={false}
-                readOnly
-                extensions={[minimalSetup, javascript({ typescript: true })]}
-              />
-            </div>
-            <a
-              href={`https://github.com/sfishel18/moment-to-temporal/issues/new?template=bug-report.yml&title=%5BBug%5D%3A+&input=${encodeURIComponent(input())}&output=${encodeURIComponent(output())}`}
-              target="_blank"
-            >
-              File a bug!
-            </a>
-          </TabPanel>
-          <TabPanel
-            value="docs"
-            class={classNames(
-              "w-full h-full p-2 rounded-lg bg-white overflow-y-auto focus:outline-none",
-              "focus:ring-2 focus:ring-offset-2 focus:ring-offset-rose-300 focus:ring-white focus:ring-opacity-60",
-            )}
-          >
-            <h1>Docs go here...</h1>
-          </TabPanel>
-        </>
-      )}
-    </TabGroup>
+            File a bug!
+          </a>
+        </div>
+      </div>
+    </AppShell>
+  );
+};
+
+const Docs = () => (
+  <AppShell>
+    <div class="w-full h-full py-8">
+      <h1>Docs go here...</h1>
+    </div>
+  </AppShell>
+);
+
+const MainSection = () => {
+  return (
+    <Router>
+      <Route path="/docs" component={Docs} />
+      <Route path="*" component={Explorer} />
+    </Router>
   );
 };
 
