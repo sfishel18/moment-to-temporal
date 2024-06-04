81d24e9d23f4bea46cab2c0317a918270de6dc71
Simon Fishel <simonf@gmail.com>
2024-06-03 19:29:54 -0700

    get icon set up, try to publish to github pages

--- docs/src/docs.tsx
+++ docs/src/docs.tsx
@@ -2,15 +2,16 @@ import { javascript } from "@codemirror/lang-javascript";
 import { debounce } from "@solid-primitives/scheduled";
 // @ts-ignore
 import { CodeMirror } from "@solid-codemirror/codemirror";
-import { Router, Route, A } from "@solidjs/router";
 import { minimalSetup } from "codemirror";
 import jsCodeShiftCore from "jscodeshift/src/core";
 import * as prettier from "prettier";
 import * as typescriptPlugin from "prettier/plugins/typescript";
 import * as estreePlugin from "prettier/plugins/estree";
-import { createSignal, createEffect, JSX } from "solid-js";
+import { createSignal, createEffect, JSX, onMount, onCleanup } from "solid-js";
 import { render } from "solid-js/web";
 import transform from "../../src/transform";
+// @ts-ignore
+import icon from "../assets/icon.png";
 import "./docs.css";
 
 function classNames(...classes: (string | boolean | undefined)[]): string {
@@ -30,39 +31,38 @@ const transpile = async (source: string) => {
 };
 
 const AppShell = (props: { children: JSX.Element }) => (
-  <div
-    class={`w-full h-screen flex flex-col bg-sky-800 bg-opacity-5 p-6`}
-  >
+  <div class={`w-full h-screen flex flex-col bg-sky-900 bg-opacity-5 p-6`}>
     <nav class="flex items-center justify-between">
       <div class="flex items-center flex-shrink-0">
-        <span class="font-semibold text-3xl tracking-tight">
+        <img src={icon} height={75} width={75} class="mr-3" />
+        <span class="font-semibold text-3xl tracking-tight text-sky-900">
           Moment &rarr; Temporal
         </span>
       </div>
       <div class="flex mr-16">
         <div class="mr-8">
-          <A
+          <a
             class={classNames(
-              "font-semibold p-1",
-              `focus:outline-none focus:text-sky-800 focus:ring-1 focus:ring-sky-800 focus:ring-offset-2 focus:rounded`,
-              `hover:text-sky-800`,
+              "font-semibold p-1 text-sky-900",
+              `focus:outline-none focus:text-sky-500 focus:ring-1 focus:ring-sky-500 focus:ring-offset-2 focus:rounded`,
+              `hover:text-sky-500`,
             )}
-            href="/explorer"
+            href="#/explorer"
           >
             Explorer
-          </A>
+          </a>
         </div>
         <div>
-          <A
+          <a
             class={classNames(
-              "font-semibold p-1",
-              `focus:outline-none focus:text-sky-800 focus:ring-1 focus:ring-sky-800 focus:ring-offset-2 focus:rounded`,
-              `hover:text-sky-800`,
+              "font-semibold p-1 text-sky-900",
+              `focus:outline-none focus:text-sky-500 focus:ring-1 focus:ring-sky-500 focus:ring-offset-2 focus:rounded`,
+              `hover:text-sky-500`,
             )}
-            href="/docs"
+            href="#/docs"
           >
             Docs
-          </A>
+          </a>
         </div>
       </div>
     </nav>
@@ -85,14 +85,14 @@ const Explorer = () => {
           <CodeMirror
             value={input()}
             onValueChange={(val: string) => setInput(val)}
-            class={`flex-1 border-sky-800 border rounded p-2 bg-white`}
+            class={`flex-1 border-sky-900 border rounded p-2 bg-white`}
             placeholder="Moment.js code goes here"
             showLineNumbers={false}
             extensions={[minimalSetup, javascript({ typescript: true })]}
           />
           <CodeMirror
             value={output()}
-            class={`flex-1 border-sky-800 border rounded p-2 bg-white`}
+            class={`flex-1 border-sky-900 border rounded p-2 bg-white`}
             placeholder="Moment.js code goes here"
             showLineNumbers={false}
             readOnly
@@ -102,9 +102,9 @@ const Explorer = () => {
         <div class="flex justify-center">
           <a
             class={classNames(
-              `py-1 px-2 ring-1 rounded ring-sky-800`,
-              `focus:outline-none focus:ring-2 focus:text-sky-800`,
-              `hover:text-sky-800 hover:ring-2`,
+              `py-1 px-2 ring-2 rounded ring-sky-900 text-sky-900`,
+              `focus:outline-none focus:ring-2 focus:text-sky-500 focus:ring-sky-500`,
+              `hover:text-sky-500 hover:ring-2 hover:ring-sky-500`,
             )}
             href={`https://github.com/sfishel18/moment-to-temporal/issues/new?template=bug-report.yml&title=%5BBug%5D%3A+&input=${encodeURIComponent(input())}&output=${encodeURIComponent(output())}`}
             target="_blank"
@@ -126,11 +126,22 @@ const Docs = () => (
 );
 
 const MainSection = () => {
+  const [fragment, setFragment] = createSignal("");
+  const onFragmentChange = () => {
+    setFragment(window.location.hash);
+  }
+  onMount(() => {
+    setFragment(window.location.hash);
+    window.addEventListener("hashchange", onFragmentChange);
+  });
+  onCleanup(() => {
+    window.removeEventListener("hashchange", onFragmentChange);
+  })
   return (
-    <Router>
-      <Route path="/docs" component={Docs} />
-      <Route path="*" component={Explorer} />
-    </Router>
+    <>
+      {fragment() === "#/docs" && <Docs />}
+      {fragment() !== "#/docs" && <Explorer />}
+    </>
   );
 };
 
